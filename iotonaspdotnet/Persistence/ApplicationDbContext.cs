52b314923c8982419fce75129f095acdabf28983
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-18 18:07:17 -0400

    Initial commit

--- iotonaspdotnet/Persistence/ApplicationDbContext.cs
+++ iotonaspdotnet/Persistence/ApplicationDbContext.cs
@@ -52,6 +52,8 @@ public class ApplicationDbContext : DbContext
             
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+
+
     }
 }
