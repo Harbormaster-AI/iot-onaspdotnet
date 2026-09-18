b36826c38749e6bb315bfe20d713c3ba6c30cc01
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-18 16:12:33 -0400

    Initial commit

--- iotonaspdotnet/Domain/Contracts/Contracts.cs
+++ iotonaspdotnet/Domain/Contracts/Contracts.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations.Schema;
 using iotonaspdotnet.Domain;
 
 namespace iotonaspdotnet.Contracts;
--- iotonaspdotnet/Domain/ValueObjects.cs
+++ iotonaspdotnet/Domain/ValueObjects.cs
@@ -1,12 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace iotonaspdotnet.Domain;
+
     [ComplexType]
     public record DeviceId(
     string Value
     );
+
     [ComplexType]
     public record FirmwareVersion(
     string Value
     );
+
     [ComplexType]
     public record Address(
     string Street,
@@ -15,16 +20,20 @@ namespace iotonaspdotnet.Domain;
     string PostalCode,
     string Country
     );
+
     [ComplexType]
     public record Uri_(
     string Value
     );
+
     [ComplexType]
     public record TopicName(
     string Value
     );
+
     [ComplexType]
     public record Checksum(
     string Algorithm,
     string Value
     );
+
